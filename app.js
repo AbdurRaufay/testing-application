66d3126564065c224bdcd9bb061766fdd88d714b
abdurrauf <abdurrauf0306@gmail.com>
2023-08-01 00:28:52 -0700

    f

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(session({
 
 app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
-  res.redirect('/');
+  res.redirect('https://dreamy-madeleine-9acb7f.netlify.app');
 });
 
 const opts = {
