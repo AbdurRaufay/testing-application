703dba0000a1d4910a7d53baba766926ee9985a5
abdurrauf <abdurrauf0306@gmail.com>
2023-07-31 18:16:23 -0700

    first

--- server/passport.js
+++ server/passport.js
@@ -23,7 +23,7 @@ passport.use(
       {
         clientID: process.env.clientID,
         clientSecret: process.env.clientSecret,
-        callbackURL: 'https://scintillating-cucurucho-8944ab.netlify.app//auth/google/callback', 
+        callbackURL: 'https://scintillating-cucurucho-8944ab.netlify.app/auth/google/callback', 
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
