ff520146836410d8489624e2b9408c18e5023471
abdurrauf <abdurrauf0306@gmail.com>
2023-07-31 19:16:40 -0700

    f

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ const opts = {
         typeDefs,
         resolvers,     
         cors: {
-          origin :"https://localhost:3000",
+          origin :"http://localhost:3000",
             // origin: 'https://prismatic-sunshine-ec4f13.netlify.app',
             credentials: true,
             methods: ['GET', 'POST'],
